75d4b8c87b27f8d21a1f76aad33077cafbbd48ee
M. J. M. FARSAN <mohammedfarsan603@gmail.com>
2025-04-17 20:37:01 +0300

    just uploaded

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayValue(val){
+    document.getElementById('display').value = document.getElementById('display').value + val;
+}function clearDisplay(){
+    document.getElementById('display').value = "";
+}function result(){
+    var userInput = document.getElementById('display').value;
+    var result = eval(userInput);
+    document.getElementById('display').value = result;
+}
